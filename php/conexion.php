bf0bfe38f05cedf544c395df706ee3a753a1f526
Sebastian Alonso <sebastian.madrueno0178@alumnos.udg.mx>
2021-04-01 20:52:07 -0600

    Acomodo

--- php/conexion.php
+++ php/conexion.php
@@ -1,12 +1,5 @@
 <?php
-    Function connectDB(){
-        $server = "localhost";
-        $user = "root";
-        $pass = "";
-        $bd = "e-commerce";
 
-        $conexion = mysql_connect($server, $user, $pass, $bd) or die ("Ha sucedido un error con la conexion");
-
-        return $conexion;
-    }
+    $conexion = mysqli_connect("localhost", "root", "", "e-commerce");
+    
 ?>
