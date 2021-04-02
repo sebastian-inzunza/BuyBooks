e4b08628ac8f61c332ee01ab0a8276d0cc89c938
Sebastian Alonso <sebastian.madrueno0178@alumnos.udg.mx>
2021-04-01 20:53:56 -0600

    Base de datos

--- e-commerce.sql
+++ e-commerce.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-04-2021 a las 22:22:13
--- Versión del servidor: 10.4.13-MariaDB
--- Versión de PHP: 7.4.8
+-- Tiempo de generación: 02-04-2021 a las 04:53:08
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -45,10 +45,17 @@ CREATE TABLE `libro` (
 
 CREATE TABLE `usuario` (
   `id` int(10) NOT NULL,
-  `nombre` text COLLATE utf16_spanish2_ci NOT NULL,
   `correo` text COLLATE utf16_spanish2_ci NOT NULL,
-  `contrasena` text COLLATE utf16_spanish2_ci NOT NULL
+  `contrasena` varchar(150) COLLATE utf16_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `correo`, `contrasena`) VALUES
+(12, '2', '40b244112641dd78dd4f93b6c9190dd46e0099194d5a44257b7efad6ef9ff4683da1eda0244448cb343aa688f5d3efd7314dafe580ac0bcbf115aeca9e8dc114'),
+(13, 'sebastian_a9@hotmail.com', '4dff4ea340f0a823f15d3f4f01ab62eae0e5da579ccb851f8db9dfe84c58b2b37b89903a740e1ee172da793a6e79d560e5f7f9bd058a12a280433ed6fa46510a');
 
 --
 -- Índices para tablas volcadas
@@ -80,7 +87,7 @@ ALTER TABLE `libro`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
