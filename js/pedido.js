5e57a8cd5eaf4e42fe17b3975870f9de88931066
Sebastian Alonso <sebastian.madrueno0178@alumnos.udg.mx>
2021-04-07 20:09:01 -0500

    prueba de error

--- js/pedido.js
+++ js/pedido.js
@@ -1,14 +1,24 @@
 const carro = new Carrito();
 const carrito = document.getElementById('carrito');
+<<<<<<< Updated upstream
 const product = document.getElementById('lista-product'); ///toma TODOOOO
 //const Listaproducto = document.querySelector('#lista-carrito ul');
 const Listaproducto = document.querySelector('#lista-carrito tbody');
+=======
+const productos = document.getElementById('lista-productos');
+const listaProductos = document.querySelector("#lista-carrito ul");
+>>>>>>> Stashed changes
 
 cargarEventos();
 
 function cargarEventos(){
+<<<<<<< Updated upstream
     product.addEventListener('click' ,(e)=>{carro.compraProducto(e)});
 
     carrito.addEventListener('click',(e)=>{carro.eliminarProducto(e)});
 
+=======
+    productos.addEventListener('click', (e) => {carro.comprarProducto(e)});
+    
+>>>>>>> Stashed changes
 }
